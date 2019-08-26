fe8e25458387a54c3898716a27041969c86171bb
Jędrzej Rzankowski <j.rzankowski@gmail.com>
2019-08-26 16:32:29 +0200

    add audio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 const masonry = new Macy({
     container: 'div.masonry',
     mobileFirst: true,
-    columns: 2
+    columns: 2,
+    breakAt: {
+        600: 3,
+
+    },
+    margin: {
+        x: 20,
+        y: 20
+    }
+
 })
+
+
+let photos = [...document.querySelectorAll(".masonry img")]
+
+
+
+let audio = document.querySelector("#audioID")
+
+//Example of an HTML Audio/Video Method
+
+const audioAdd = () => {
+    const audioChanger = () => {
+        let audioNumber = Math.floor(Math.random() * 52);
+        let audioFinalSource = `audio/glitch${audioNumber}.wav`
+        return audioFinalSource;
+    }
+
+
+    document.getElementById("audioID").setAttribute('src', `${audioChanger()}`);
+    audio.play();
+}
+
+photos.forEach(img => {
+    img.addEventListener("mouseover", audioAdd)
+
+});
